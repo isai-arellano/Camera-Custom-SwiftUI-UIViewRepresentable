e81bafca4e30ac08d0d7ec3f2e6e7ccfae62ba5f
Isaí Arellano <isai.arellano@outlook.com>
2022-06-15 18:33:11 -0500

    Initial Commit

---
+++ camera-with-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  camera-with-swiftui
+//
+//  Created by Isaí on 15/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ camera-with-swiftui/camera_with_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_with_swiftuiApp.swift
+//  camera-with-swiftui
+//
+//  Created by Isaí on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct camera_with_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
