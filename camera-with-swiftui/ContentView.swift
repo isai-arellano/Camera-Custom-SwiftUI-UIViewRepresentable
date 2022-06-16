948b5c616cd91016ef7ea4d3b16a294c3478217b
Isaí Arellano <isai.arellano@outlook.com>
2022-06-15 21:56:52 -0500

    Inital commit

--- camera-with-swiftui/ContentView.swift
+++ camera-with-swiftui/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        /*Text("Hello, world!")
+            .padding()*/
+        CameraView()
     }
 }
 
@@ -18,4 +20,208 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+// Vista previa de camara
+struct CameraView : View {
+    
+    @StateObject var camera = CameraModel()
+    
+    var body: some View {
+        ZStack  {
+            CameraPreview(camera: camera)
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack {
+                if camera.isTaken {
+                    HStack {
+                        Spacer()
+                        Button(action: camera.reTake, label: {
+                            Image(systemName: "arrow.triangle.2.circlepath.camera")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        })
+                        .padding(.trailing, 10)
+                    }
+                }
+                Spacer()
+                HStack{
+                    if camera.isTaken {
+                        Button(action: { if !camera.isSaved{camera.savePicture()}}, label: {
+                            Text(camera.isSaved ? "Saved" : "Save")
+                                .foregroundColor(.black)
+                                .fontWeight(.semibold)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 20)
+                                .background(Color.white)
+                                .clipShape(Capsule())
+                            
+                        })
+                        .padding(.leading)
+                        Spacer()
+                    }else{
+                        Button(action: camera.takePic, label: {
+                            ZStack {
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 65, height: 65)
+                                
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 2)
+                                    .frame(width: 75, height: 75)
+                            }
+                        })
+                    }
+                }
+                .frame(height: 75)
+            }
+        }
+        .onAppear(perform: {
+            camera.Check()
+        })
+    }
+}
+
+
+// Modelo de camara
+
+class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
+    
+    @Published var isTaken = false
+    
+    @Published var session = AVCaptureSession()
+    
+    @Published var alert = false
+    
+    @Published var output = AVCapturePhotoOutput()
+    
+    @Published var preview : AVCaptureVideoPreviewLayer!
+    
+    @Published var isSaved = false
+    
+    @Published var picData = Data(count: 0)
+    
+    func Check(){
+        // Revisar permisos de camara
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .authorized:
+            setUp()
+            return
+        // Configuración de session
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { (status) in
+                if status {
+                    self.setUp()
+                }
+            }
+        case .denied:
+            self.alert.toggle()
+            return
+            
+        default:
+            return
+        }
+    }
+    
+        func setUp(){
+            do {
+                // Configuraciones de camara
+                self.session.beginConfiguration()
+                // Seleccion de camara
+                let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back)
+                
+                let input = try AVCaptureDeviceInput(device: device!)
+                
+                //Revisar y agregar session
+                if self.session.canAddInput(input){
+                    self.session.addInput(input)
+                }
+                
+                //Salida
+                if self.session.canAddOutput(self.output){
+                    self.session.addOutput(self.output)
+                }
+                
+                self.session.commitConfiguration()
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+            
+        }
+    
+    // Capturar fotos
+
+    func takePic(){
+        DispatchQueue.global(qos: .background).async {
+            self.output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+            //self.session.stopRunning()
+            DispatchQueue.main.async {
+                withAnimation{self.isTaken.toggle()}
+            }
+        }
+    }
+    
+    func reTake(){
+        DispatchQueue.global(qos: .background).async {
+            self.session.startRunning()
+            
+            DispatchQueue.main.async {
+                withAnimation{self.isTaken.toggle()}
+                
+                // Limpiar
+                self.isSaved = false
+                
+            }
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if error != nil {
+            return
+        }
+        print("Pic taken")
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        self.picData = imageData
+    }
+    
+    func savePicture(){
+        let image = UIImage(data: self.picData)!
+        // Guardar imagen
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
+        self.isSaved = true
+        
+        print("Saved successfully")
+        
+    }
+ }
+
+  // Configuración de vista
+
+struct CameraPreview: UIViewRepresentable {
+    @ObservedObject var camera : CameraModel
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+        camera.preview.frame = view.frame
+        
+        camera.preview.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(camera.preview)
+        
+        // Iniciar session
+        camera.session.startRunning()
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
 }
